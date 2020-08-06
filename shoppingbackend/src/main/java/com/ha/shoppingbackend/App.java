e0eac0d6d8695966ead92c2abfeac5c4ab7bafa8
Harish Algot <harishalgot92@gmail.com>
2020-08-06 15:25:40 +0200

    my project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/ha/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.ha.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
